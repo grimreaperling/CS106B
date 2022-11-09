1d9cf60dadee8aa36b4d28430c0c6d2c4b5cbd2f
Herbert Yao <grimreaperling@gmail.com>
2022-11-09 16:17:04 +0800

    Finish the first assignment!

--- assignments/starter-assign1/soundex.cpp
+++ assignments/starter-assign1/soundex.cpp
@@ -32,15 +32,90 @@ string removeNonLetters(string s) {
     return result;
 }
 
+/*
+ * This program is to encode the input string.
+ */
+string encode(string s) {
+    string s1 = "AEIOUHWY";
+    string s2 = "BFPV";
+    string s3 = "CGJKQSXZ";
+    string s4 = "DT";
+    string s5 = "MN";
+    string res = "";
+    for (char c : s) {
+        c = toupper(c);
+        if (s1.find(c) != string::npos) res += '0';
+        else if (s2.find(c) != string::npos) res += '1';
+        else if (s3.find(c) != string::npos) res += '2';
+        else if (s4.find(c) != string::npos) res += '3';
+        else if (c == 'L') res += '4';
+        else if (s5.find(c) != string::npos) res += '5';
+        else res += '6';
+    }
+    return res;
+}
 
-/* TODO: Replace this comment with a descriptive function
- * header comment.
+/*
+ * The function to coalesce the string.
+ */
+string coalesce(string s) {
+    if (s.size() == 1) return s;
+    if (s[0] == s[1]) {
+        if (s.size() == 2) return s.substr(1);
+        return coalesce(s.substr(1));
+    } else {
+        return s[0] + coalesce(s.substr(1));
+    }
+}
+
+/*
+ * Replace the first element with the first character.
+ */
+string replace(string s, char c) {
+    c = toupper(c);
+    return s.replace(0, 1, 1, c);
+}
+
+/*
+ * Remove all the zero in the string.
+ */
+string removeZero(string s) {
+    if (s.size() == 0) return "";
+    if (s[0] == '0') {
+        return removeZero(s.substr(1));
+    }
+    return s[0] + removeZero(s.substr(1));
+}
+
+/*
+ * The function to make the length of the string exact four.
+ */
+string makeFour(string s) {
+    if (s.size() > 4) return s.substr(0, 4);
+    if (s.size() < 4) {
+        int i = 4 - s.size();
+        while (i > 0) {
+            s += '0';
+            i = i - 1;
+        }
+    }
+    return s;
+}
+
+/* 
+ * The function take in a string and return the soundex of it.
  */
 string soundex(string s) {
     /* TODO: Fill in this function. */
-    return "";
+    string res;
+    res = removeNonLetters(s);
+    res = encode(res);
+    res = coalesce(res);
+    res = replace(res, s[0]);
+    res = removeZero(res);
+    res = makeFour(res);
+    return res;
 }
-
 
 /* TODO: Replace this comment with a descriptive function
  * header comment.
@@ -59,8 +134,22 @@ void soundexSearch(string filepath) {
 
     // The names in the database are now stored in the provided
     // vector named databaseNames
-
-    /* TODO: Fill in the remainder of this function. */
+    while(1) {
+        string name = getLine("Enter a surname (RETURN to quit):");
+        if (name == "") {
+            cout << "All done!" << endl;
+            return;
+        }
+        string code = soundex(name);
+        cout << "Soundex code is " << code << endl;
+        Vector<string> res;
+        for (string s : databaseNames) {
+            string dex = soundex(s);
+            if (dex == code) res.add(s);
+        }
+        res.sort();
+        cout << res << endl;
+    }
 }
 
 
@@ -131,9 +220,44 @@ PROVIDED_TEST("Ashcraft is not a special case") {
     EXPECT_EQUAL(soundex("Ashcraft"), "A226");
 }
 
-STUDENT_TEST("Some special case to exposes the hidden problem of the removeNonLetters function") {
+STUDENT_TEST("Some special case to exposes the hidden problem of the removeNonLetters function.") {
     EXPECT_EQUAL(removeNonLetters("'Huge"), "Huge");
 }
 
+STUDENT_TEST("Test the encode function!") {
+    EXPECT_EQUAL(encode("Curie"), "20600");
+}
 
+STUDENT_TEST("Test the encode function on some more cases!") {
+    EXPECT_EQUAL(encode("Huge"), "0020");
+}
 
+STUDENT_TEST("Test on the coalesce function!") {
+    EXPECT_EQUAL(coalesce("001100"), "010");
+}
+
+STUDENT_TEST("Test on the coalesce function on some more cases!") {
+    EXPECT_EQUAL(coalesce("555555545561233"), "5456123");
+    EXPECT_EQUAL(coalesce("555671"), "5671");
+}
+
+STUDENT_TEST("Test on the replace function!") {
+    EXPECT_EQUAL(replace("1234", 'c'), "C234");
+}
+
+STUDENT_TEST("Test on the replace function!") {
+    EXPECT_EQUAL(replace("645241234", 'c'), "C45241234");
+}
+
+STUDENT_TEST("Test on the remove function!") {
+    EXPECT_EQUAL(removeZero("005671"), "5671");
+}
+
+STUDENT_TEST("Test on the remove function!") {
+    EXPECT_EQUAL(removeZero("0056007100"), "5671");
+}
+
+STUDENT_TEST("Test on the makeFour function!") {
+    EXPECT_EQUAL(makeFour("C92"), "C920");
+    EXPECT_EQUAL(makeFour("C92231"), "C922");
+}
